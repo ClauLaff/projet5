e0bf196e6096cd47caaa1b4a9d1603c5f6ecd697
claudia <claudialaffargue@outlook.com>
2025-05-06 22:32:08 +0200

    Ajout de react router et structuration du projet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
-import App from './components/App.jsx'
+import App from './pages/Home/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
