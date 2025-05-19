354376b88c36cae0ba9245f647b0ab65d9072b3b
claudia <claudialaffargue@outlook.com>
2025-05-20 00:37:47 +0200

    Création de la page d'erreur

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,15 @@
-function Error() {
-  
+import './Error.scss';
+import {Link} from "react-router";
 
-    return (
-     <div>404 Cette page d'existe pas</div>
-    )
+function Error() {
+
+  return (
+    <div className="error-page">
+      <div className="error-code">404</div>
+      <div className="error-text">Oups! La page que vous demandez n'existe pas!</div>
+      <Link className="error-lien-accueil" to="/">Retourner sur la page d'accueil</Link>
+    </div>
+  )
   }
   
   export default Error
